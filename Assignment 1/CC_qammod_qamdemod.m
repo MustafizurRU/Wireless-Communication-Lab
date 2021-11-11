f90e5bc6b269d98a2f632429e2e2d724cc0a9917
Md.Mustafizur Rahman <33628705+MustafizurRU@users.noreply.github.com>
2021-11-11 14:54:41 +0600

    Add files via upload

---
+++ Assignment 1/CC_qammod_qamdemod.m
@@ -0,0 +1,116 @@
+clc
+clear all
+close all
+BERLop = [];
+times =10;
+mpsk = [];
+for i = 2:5
+    %bps
+    M = 2^i;
+    bps = log2(M);
+    BERLoop = [];
+    for j = 0: times
+        %% input +reshape
+        nosymbol = 1000;
+        symbols = randint(1,nosymbol,256);
+        symbolToBitMapping = de2bi(symbols,8,'left-msb');
+        totNoBits = numel(symbolToBitMapping);
+        inputReshapedBits = reshape(symbolToBitMapping,1,totNoBits);
+        %% padding
+        remainder = rem(totNoBits,bps);
+        if(remainder == 0)
+            userPaddedData = inputReshapedBits;
+        else
+            paddingBits = zeros(1,bps-remainder);
+            userPaddedData = [inputReshapedBits  paddingBits];
+        end
+        
+        %% cc 
+        trellis = poly2trellis(3,[7 3]);
+        ccEncoded = convenc(userPaddedData,trellis);
+        
+        state = 48321;
+        ccEncodedIntrlv = randintrlv(ccEncoded,state);
+        
+        %% Qam Modulation
+        reshapedUserPaddedData = reshape(ccEncodedIntrlv,numel(ccEncodedIntrlv)/bps,bps);
+        bitToSymbolMapping = bi2de(reshapedUserPaddedData,'left-msb');
+        modulatedSymbol = qammod(bitToSymbolMapping,M);
+        
+        %% Use Ricianchan Chanel 
+        SNR = [];
+        BER = [];
+        for snr =0:15
+            SNR = [SNR snr];
+            ts = 1/totNoBits;
+            fd = 100;
+            kk = 15;
+            
+%             chan = rayleighchan(ts,fd);
+%             absForm = abs(filter(chan,ones(size(modulatedSymbol))));
+%             fadedSignal = absForm .* modulatedSymbol;
+            chan = ricianchan(ts,fd,kk);
+            fadedSignal = filter(chan,modulatedSymbol);
+            
+            noisySymbols = awgn(fadedSignal,snr,'measured');
+            %% use qamdemod
+            demodulatedSymbol = qamdemod(noisySymbols,M);
+            
+            demodulatedSymbolToBitMapping = de2bi(demodulatedSymbol,'left-msb');
+            reshapedDemodulatedBits = reshape(demodulatedSymbolToBitMapping,1,numel(demodulatedSymbolToBitMapping));
+            
+            %% CC 
+            state = 48321;
+            ccDeintrlv = randdeintrlv(reshapedDemodulatedBits,state);
+            tblen = 3;
+            opmode = 'trunc';
+            dectype = 'hard';
+            ccDecoded = vitdec(ccDeintrlv,trellis,tblen,opmode,dectype);
+            
+            %% remove pading
+            demodulatedBitsWithoutPadding = ccDecoded(1:totNoBits);
+            [noe ber] = biterr(inputReshapedBits,demodulatedBitsWithoutPadding);
+            BER = [BER ber];
+            
+            %% Original Text
+            %{
+            txtBits =
+            reshape(demodulatedBitsWithoutPadding,numel(demodulatedBitsWithoutPadding)/8,8);
+            txtBitsDecimal = bi2de(tstBits,'left-msb');
+            msg = char(txtBitsDecimal)';
+            %}
+        end
+        BERLoop = [BERLoop; BER];
+        
+    end
+    BERLop = sum(BERLoop)/times;
+    mpsk = [mpsk;BERLop];
+end
+    figure(1)
+    semilogy(SNR,mpsk(1,:),'-o',SNR,mpsk(2,:),'-o',SNR,mpsk(3,:),'-o');
+    legend('BPSK','QPSK','8PSK')
+    xlabel('SNR')
+    ylabel('BER')
+    title('SNR Vs BER')
+            
+            
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
